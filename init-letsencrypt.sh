3d157433e7901da056652ef45ecf842f7a1f46e0
Shashank Jha <shashankjha1994@gmail.com>
2023-12-03 15:08:13 +0530

    initial running app

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -39,7 +39,7 @@ echo
 
 
 echo "### Starting nginx ..."
-docker compose up --force-recreate -d nginx
+docker compose up --force-recreate -d odyssey_nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
