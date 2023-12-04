96baff832e2df22e7d53036405344a7bf124e334
Shashank Jha <shashankjha1994@gmail.com>
2023-12-04 13:57:56 +0530

    letsencrypt setup

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 python manage.py migrate
 
 # Start Uvicorn with specified workers and uvloop
-uvicorn odyssey.asgi:application --host 0.0.0.0 --port 8000 --workers 4 --loop uvloop --http h11
+uvicorn odyssey.asgi:application --host 0.0.0.0 --port 8002 --workers 4 --loop uvloop --http h11
